54b72c29fbd564dd3f7233680f8ad2db8fdb4971
rickaa <37962604+d0l-phin@users.noreply.github.com>
2020-11-16 12:59:15 +0100

    📝 update

--- 00_zero/03_x01.py
+++ 00_zero/03_x01.py
@@ -1,3 +1,4 @@
+# Opcion 1
 altura_actual = 0.5
 altura_torre = 114
 altura_caja = 0.5
@@ -7,3 +8,29 @@ while altura_actual < altura_torre:
     cajas *= 2
     
     altura_actual = cajas * altura_caja
+    
+    
+# Opcion 2
+num_dias = 1
+num_cajas = 1
+
+# variables medidas en metros
+altura_torre = 114
+tamaño_caja = 0.5
+altura_cajas = tamaño_caja * num_cajas
+
+print("Empezamos con una altura de cajas:", altura_cajas)
+
+### Variables
+
+while altura_cajas < altura_torre:
+    num_dias += 1
+    print("Dia", num_dias)
+
+    num_cajas *= 2
+    print("Cajas", num_cajas)
+
+    altura_cajas = tamaño_caja * num_cajas
+    print("Altura", altura_cajas)
+
+## CODE
--- 00_zero/05_x01.py
+++ 00_zero/05_x01.py
@@ -1,6 +1,18 @@
+# Opcion 1
 for nombre, valores in equipos.items():
-        
+
     if sum(valores) > 200:
         print("el pc", nombres_pc[nombre], "tiene mas de 200")
-    
+
+
+# Opcion 2
+for nombre, valores in equipos.items():
+    print(f"Iterando sobre {nombre}")
+    print(f"Iterando con {valores}")
+
     suma = 0
+    for v in valores:
+        suma += v
+
+    if suma > 200:
+        print("el pc", nombres_pc[nombre], "tiene mas de 200")
