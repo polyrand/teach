31b6c702c68051290f56dddc2bd2d41bafdf59c7
rickaa <37962604+d0l-phin@users.noreply.github.com>
2021-02-17 12:59:47 +0100

    update docs and json nb

--- 02_json-yaml/mis_funciones.py
+++ 02_json-yaml/mis_funciones.py
@@ -1,16 +1,21 @@
-
 import json
 
 
 def leer_json(nombre):
+    
     with open(nombre) as f:
         contenidos_archivo = f.read()
-        obj = json.loads(contenidos_archivo)
+    obj = json.loads(contenidos_archivo)
 
     return obj
 
 
 def guardar_json(obj, nombre):
+    
+    cadena_json = json.dumps(obj)
+    
     with open(nombre, "w") as f:
-        cadena_json = json.dumps(obj)
+        
         f.write(cadena_json)
+        
+    return cadena_json
